c91eb80d9ec950c85bab17a2cca8e3ce194d3ff0
mikha-el82 <mikhael@centrum.cz>
2021-12-02 13:31:07 +0100

    Created Food class + collision of the snake with food detection

---
+++ food.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("green")
+        self.speed("fastest")  # without animation
+        rand_x = 20 * random.randint(-14, 14)  # TODO #2 use screen size to get the range
+        rand_y = 20 * random.randint(-14, 14)
+        self.goto(rand_x, rand_y)
+
+    def refresh(self):
+        rand_x = 20 * random.randint(-14, 14)
+        rand_y = 20 * random.randint(-14, 14)
+        self.goto(rand_x, rand_y)
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+
 import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=600, height=600)  # TODO #1 Include screen width in food to get random location
 screen.bgcolor("black")
 screen.title("Udemy Snake Game")
 screen.tracer(0)  # sets turtle animation off
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -25,9 +29,13 @@ screen.update()
 
 # moving of the rest of the snake
 while game_is_on:
-    time.sleep(1)
+    time.sleep(0.5)
     snake.move()
     screen.update()
     # game_is_on = False
+
+    if snake.head.distance(food) < 10:
+        print("nom nom nom")
+        food.refresh()
 
 screen.exitonclick()
